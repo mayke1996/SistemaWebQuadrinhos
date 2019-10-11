dec6168bc900922fcddf6554897e1e4f8a1c5520
Mayke <maykeferreiramonica@gmail.com>
2019-10-11 15:45:41 -0300

    Todas modificacoes de integracao com o banco

--- QuadrinhosProject/quandrinhosApp/admin.py
+++ QuadrinhosProject/quandrinhosApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import info_quadrinhos, usuario
+
+admin.site.register(info_quadrinhos)
+admin.site.register(usuario)
+
 # Register your models here.
